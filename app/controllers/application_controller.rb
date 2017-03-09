3f543e37c38c50d55ad21be9b0e7d71f2b79d7ed
Rose Karr <rose@rosalinekarr.com>
2017-03-09 11:16:49 -0500

    Changed devise to redirect to referrer on sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :store_current_location, :unless => :devise_controller?
+
+  protected
+
   def authenticate_user!
     redirect_to user_reddit_omniauth_authorize_path unless user_signed_in?
+  end
+
+  private
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
+  def after_sign_out_path_for(resource)
+    request.referrer || root_path
   end
 end
