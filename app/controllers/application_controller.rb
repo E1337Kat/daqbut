98b3ea43a0529b2f957d55e15b30b834e7139ad0
Rose Karr <rose@rosalinekarr.com>
2017-03-08 20:17:24 -0500

    Added basic meme controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def authenticate_user!
+    redirect_to user_reddit_omniauth_authorize_path unless user_signed_in?
+  end
 end
---
+++ app/controllers/memes_controller.rb
@@ -0,0 +1,64 @@
+class MemesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @memes = Meme.order(price: :desc)
+  end
+
+  def show
+    @meme = Meme.find_by(slug: params[:id])
+  end
+
+  def new
+    @meme = current_user.memes.new
+  end
+
+  def edit
+    @meme = current_user.memes.find_by(slug: params[:id])
+  end
+
+  def create
+    @meme = current_user.memes.new meme_params
+    if @meme.save
+      respond_to do |f|
+        f.html { redirect_to meme_path(@meme.slug), notice: 'IPO launched!' }
+        f.json { render 'show' }
+      end
+    else
+      respond_to do |f|
+        f.html { render 'new' }
+        f.json { render 'show' }
+      end
+    end
+  end
+
+  def update
+    @meme = current_user.memes.find_by(slug: params[:id])
+    if @meme.update(meme_params)
+      respond_to do |f|
+        f.html { redirect_to meme_path(@meme.slug), notice: 'IPO successfully launched!' }
+        f.json { render 'show' }
+      end
+    else
+      respond_to do |f|
+        f.html { render 'edit' }
+        f.json { render 'show' }
+      end
+    end
+  end
+
+  def destroy
+    @meme = current_user.memes.find_by(slug: params[:id])
+    @meme.destroy
+    respond_to do |f|
+      f.html { redirect_to memes_path, notice: 'Meme successfully cashed out!' }
+      f.json { render json: { success: true } }
+    end
+  end
+
+  private
+
+  def meme_params
+    params.require(:meme).permit(:title, :image)
+  end
+end
