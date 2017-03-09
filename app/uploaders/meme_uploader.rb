5d666c0ffd4baa2469247199f28fb35bab83c853
Rose Karr <rose@rosalinekarr.com>
2017-03-09 10:21:45 -0500

    Added a whitelist for acceptable filetypes

--- app/uploaders/meme_uploader.rb
+++ app/uploaders/meme_uploader.rb
@@ -10,4 +10,12 @@ class MemeUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  def content_type_whitelist
+    /image\//
+  end
 end
