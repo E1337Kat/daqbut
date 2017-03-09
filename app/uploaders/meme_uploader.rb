57bc01765e1a041edef2fdcb300628ed4352daf6
Rose Karr <rose@rosalinekarr.com>
2017-03-09 10:16:41 -0500

    Added multiple sizes to meme image uploader

--- app/uploaders/meme_uploader.rb
+++ app/uploaders/meme_uploader.rb
@@ -1,4 +1,12 @@
 class MemeUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  process resize_to_fit: [640, 640]
+
+  version :thumb do
+    process resize_to_fill: [64, 64]
+  end
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
