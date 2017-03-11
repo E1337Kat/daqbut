731268ab8599dfd99d74ff93740f6dd02235f351
Rose Karr <rose@rosalinekarr.com>
2017-03-11 15:04:55 -0500

    Removed automatic updating of meme prices

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,12 +1,4 @@
 class Share < ApplicationRecord
   belongs_to :meme, counter_cache: true
   belongs_to :user, counter_cache: true
-
-  after_create :update_meme
-
-  private
-
-  def update_meme
-    meme.save
-  end
 end
