269805ebc22d07e3b04a8d156762906d92d85d5a
Rose Karr <rose@rosalinekarr.com>
2017-03-12 20:42:00 -0400

    Fixed bug in image uploads

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
       aws_access_key_id:     ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
     }
-    config.fog_directory  = 'daqbut'
+    config.fog_directory  = ENV['BUCKETEER_BUCKET_NAME']
     config.fog_public     = false
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
     config.storage :fog
