b4b574b87e38a4eef1d404c24f5df3b102dfc1a4
Rose Karr <rose@rosalinekarr.com>
2017-03-18 12:28:23 -0400

    Fixed 404 bug on chart images

--- app/controllers/chart_controller.rb
+++ app/controllers/chart_controller.rb
@@ -1,6 +1,6 @@
 class ChartController < ApplicationController
   def show
-    raise ActiveRecord::NotFoundError if $redis.llen(params[:meme_id]) == 0
+    raise ActiveRecord::RecordNotFound if $redis.llen(params[:meme_id]) == 0
     data = $redis.lrange(params[:meme_id], -100, -1).map(&:to_i)
     g = Gruff::Line.new((params[:width] || 304).to_i, false)
     g.theme = {
