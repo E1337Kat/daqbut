984731a71ec31c4a341913b1ded103c722199fdc
Rose Karr <rose@rosalinekarr.com>
2017-03-23 13:10:52 -0400

    Fixed permissions bug in chart svg

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,5 +1,5 @@
 class MemesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :chart]
   before_action :store_current_location
 
   def index
