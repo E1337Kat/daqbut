dbaf79915fe5877debaca51fff16fb58f35ec596
Rose Karr <rose@rosalinekarr.com>
2017-03-23 18:48:04 -0400

    Fixed bug in charts for new memes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,8 +10,10 @@ module ApplicationHelper
 
   def chart_points(meme)
     raw_points = $redis.lrange(meme.slug, -100, -1).map(&:to_i)
+    span = (raw_points.max - raw_points.min)
+    return '' if span == 0
     point_strings = raw_points.map.with_index do |price, index|
-      value = (price - raw_points.min) * 100.0 / (raw_points.max - raw_points.min)
+      value = (price - raw_points.min) * 100.0 / span
       "#{index},#{value.round}"
     end
     point_strings.join(' ')
